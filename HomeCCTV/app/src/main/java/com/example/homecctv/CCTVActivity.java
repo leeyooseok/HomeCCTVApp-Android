c16d9d05a2c2c79d0c9005fd3b6793c2d1d804c8
cctv <zldzk2908@naver.com>
2024-09-12 18:24:01 +0900

    CCTV

--- HomeCCTV/app/src/main/java/com/example/homecctv/CCTVActivity.java
+++ HomeCCTV/app/src/main/java/com/example/homecctv/CCTVActivity.java
@@ -3,6 +3,7 @@ package com.example.homecctv;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +14,10 @@ public class CCTVActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cctv);
+        // 뒤로가기 버튼 설정
+        Button buttonBack = findViewById(R.id.backmainBTN);
+
+        buttonBack.setOnClickListener(v -> openMainlActivity());
 
         StreamCCTV cctvSurfaceView1 = findViewById(R.id.cctvSurfaceView1);
         StreamCCTV cctvSurfaceView2 = findViewById(R.id.cctvSurfaceView2);
@@ -20,7 +25,7 @@ public class CCTVActivity extends AppCompatActivity {
 
         // 각 뷰에 다른 URL 설정
         cctvSurfaceView1.setStreamUrl("http://192.168.0.109:8000/camera/mjpeg");
-        cctvSurfaceView2.setStreamUrl("http://192.168.0.100:8000/camera/mjpeg");
+        cctvSurfaceView2.setStreamUrl("http://192.168.0.109:8000/camera/mjpeg");
         cctvSurfaceView3.setStreamUrl("http://109.236.111.203/mjpg/video.mjpg");;
 
         // 첫 번째 CCTV 클릭 시
@@ -38,6 +43,12 @@ public class CCTVActivity extends AppCompatActivity {
         Intent intent = new Intent(this, CCTVControlActivity.class);
         intent.putExtra("streamUrl", url);
         intent.putExtra("cameraIP", IP);
+        startActivity(intent);
+    }
+    private void openMainlActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        // 새 액티비티 시작
         startActivity(intent);
     }
 }
